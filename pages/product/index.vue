83346b0fdd690245738742d939242f32e9203de3
oni113 <miskw99@gmail.com>
2022-12-12 16:15:52 +0900

    display product list

--- pages/product/index.vue
+++ pages/product/index.vue
@@ -1,10 +1,17 @@
 <template>
-    <div>
-        <ul>
-            <li v-for="product in products" v-bind:key="product.id">
-                {{ product.name }} {{ product.price }}
-            </li>
-        </ul>
+    <div class="app">
+        <main>
+            <div>
+                <input type="text"/>
+            </div>
+            <ul>
+                <li v-for="product in products" :key="product.id" class="item flex">
+                    <img :src="product.imageUrl" :alt="product.name" class="product-image"/>
+                    <p>{{ product.name }}</p>
+                    <span>{{ product.price }}</span>
+                </li>
+            </ul>
+        </main>
     </div>
 </template>
 
@@ -14,15 +21,15 @@ import axios from 'axios'
 export default {
     async asyncData() {
         // 페이지가 그려지기 전에 (컴포넌트 생성 전에) 실행되기 때문에 this 접근 안됨
-        const rs = await axios.get('http://localhost:3000/products');
-        const products = rs.data;
+        const respons = await axios.get('http://localhost:3000/products');
+        const products = respons.data.map(product => ({
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            imageUrl: `${product.imageUrl}?random=${Math.random()}` 
+        }));
         return {
             products
-        }
-    },
-    data() {
-        return {
-            products: []
         }
     },
     head : {
@@ -30,3 +37,37 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
