4ac07b168ef40749aa5cce4f19730865fa8df1c4
oni113 <miskw99@gmail.com>
2023-01-02 16:21:36 +0900

    customize style in cart page

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="app">
-      <main>
-          <p>Cart</p>
-          <ul>
-              <li v-for="cartItem in $store.state.cartItems" :key="cartItem.id" class="item flex" >
-                  <img :src="cartItem.imageUrl" :alt="cartItem.name" class="product-image"/>
-                  <p>{{ cartItem.name }}</p>
-                  <span>{{ cartItem.price }}</span>
-              </li>
-          </ul>
-          <div class="cart-wrapper">
-            <button class="btn" @click="moveToProduct">Move to Product</button>
-          </div>
-      </main>
+  <div class="container">
+    <h1 class="list-title">Cart</h1>
+    <div class="list-wrapper">
+      <ul>
+        <li v-for="cartItem in $store.state.cartItems" :key="cartItem.id" class="list-item" >
+            <img :src="cartItem.imageUrl" :alt="cartItem.name" class="thumbnail"/>
+            <div class="description">
+              <p>{{ cartItem.name }}</p>
+              <span>{{ cartItem.price }}</span>
+            </div>
+        </li>
+      </ul>
+    </div>
+    <div class="extra-panel">
+      <button class="btn">Purcharse</button>
+      <button class="btn" @click="moveToProduct">Move to Product</button>
+    </div>
   </div>
 </template>
 
@@ -27,32 +30,31 @@ export default {
 </script>
 
 <style scoped>
-.flex {
+.container {
+  margin: 2rem 10rem;
+}
+.list-title {
+  font-weight: 700;
+  font-size: 1.4rem;
+}
+.list-wrapper {
+  margin: 0.4rem 0;
+}
+.list-item {
   display: flex;
-  justify-content: center;
 }
-.item {
-  display: inline-block;
-  width: 400px;
-  height: 345px;
-  text-align: center;
-  margin: 0 0.5rem;
-  cursor: pointer;
+.thumbnail {
+  width: 100px;
+  height: 100px;
 }
-.product-image {
-  width: 400px;
-  height: 250px;
+.description {
+  padding: 2rem 1rem;
 }
-.app {
-  position: relative;
+.extra-panel {
+  text-align: right;
+  padding: 0.2rem 3rem;
 }
-.cart-wrapper {
-  position: sticky;
-  float: right;
-  bottom: 50px;
-  right: 50px;
-}
-.cart-wrapper .btn {
+.extra-panel .btn {
   display: inline-block;
   height: 40px;
   font-size: 1rem;
